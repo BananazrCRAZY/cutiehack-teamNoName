d2e9160200895b8ae4e2440a40b4115912051e3f
BananazrCRAZY <bananamandoge@gmail.com>
2024-11-16 12:05:42 -0800

    rm category

--- Category.h
+++
@@ -1,9 +0,0 @@
-// #include "Category.h"
-// #include <iostream>
-// #include "stdlib.h"
-// using namespace std;
-
-// Category::Category(double budget, string name) : _budgetPercent(budget), _category(name) {}
-// setPercent(double percent);
-// setAmount(double income);
-// amount();
