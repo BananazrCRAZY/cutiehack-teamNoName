b31aef8a827d27ec11242a1d9420d20f62716f6c
stricker1 <stricker1392@gmail.com>
2024-11-16 13:26:33 -0800

    added Category.h

--- Category.h
+++ Category.h
@@ -1,9 +1,43 @@
-// #include "Category.h"
-// #include <iostream>
-// #include "stdlib.h"
-// using namespace std;
+#ifndef CATEGORY_H
+#define CATEGORY_H
 
-// Category::Category(double budget, string name) : _budgetPercent(budget), _category(name) {}
-// setPercent(double percent);
-// setAmount(double income);
-// amount();
+#include <iostream>
+#include <string>
+#include <vector>
+#include "Item.h"
+
+using namespace std;
+
+class Category{
+    private:
+        string _categoryName;
+        vector<Item> _items;
+        double _total;
+        double _percentBudget;
+        double _userIncome;
+        double _remaining;
+
+    public:
+        Category(string nameOfCategory = "NO_CATEGORY", double total = -1, double percentBudget = -1, double userIncome = -1);
+        
+        // getters
+        string getCategoryName() const;
+        double getTotal() const;
+        double percentBudget() const;
+        double getRemaining() const;
+
+        // setters
+        void setCategoryName(string nameOfCategory);
+        void setTotal(double totalAmount);
+        void setPercentBudget(double percentOfBudget);
+        void setUserIncome(double newUserIncome);
+
+        // mutators
+        void add(Item &item);
+        void remove_back();
+
+        friend ostream & operator<<(ostream &o, const Category &rhs);
+};
+
+
+#endif
