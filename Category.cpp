89a20ce61b6d6928f4559b24b12e19492b6b47fe
stricker1 <stricker1392@gmail.com>
2024-11-16 13:27:27 -0800

    added Category.cpp

--- Category.cpp
+++ Category.cpp
@@ -1,20 +1,54 @@
-// #ifndef CATEGORY_H
-// #define CATEGORY_H
+#include "Category.h"
 
-// #include <iostream>
-// #include "stdlib.h"
-// using namespace std;
+Category::Category(string nameOfCategory, double total, double percentBudget, double userIncome) : _categoryName(nameOfCategory), _total(total), _percentBudget(percentBudget), _userIncome(userIncome), _remaining((_userIncome*_percentBudget)/100){}
 
-// class Category {
-//     private:
-//         double _budgetPercent;
-//         double _budgetAmount;
-//         string _categoryName;
-//     public:
-//         Category(double budget = 0, string name = "empty");
-//         setPercent(double percent);
-//         setAmount(double income);
-//         amount();
-// }
+// getters
+string Category::getCategoryName() const {
+    return this->_categoryName;
+}
+double Category::getTotal() const {
+    return this->_total;
+}
+double Category::percentBudget() const {
+    return this->_percentBudget;
+}
+double Category::getRemaining() const {
+    return this->_remaining;
+}
 
-// #endif
+// setters
+void Category::setCategoryName(string nameOfCategory) {
+    this->_categoryName = nameOfCategory;
+}
+void Category::setTotal(double totalAmount) {
+    this->_total = totalAmount;
+}
+void Category::setPercentBudget(double percentOfBudget) {
+    this->_percentBudget = percentOfBudget;
+}
+
+// mutators
+
+/*
+    Adds an Item to the back of the vector of Items. Calls push_back() from the vector class
+*/
+void Category::add(Item &item) {
+    this->_items.push_back(item);
+}
+
+/*
+    Removes the most recently added Item from the vector of Items. Calls pop_back() from the vector class
+*/
+void Category::remove_back() {
+    this->_items.pop_back();
+}
+
+ostream & operator<<(ostream &o, const Category &rhs) {
+
+    for (unsigned i = 0; i < rhs._items.size(); i++) {
+        o << rhs.getCategoryName() << ": " << endl;
+        o << rhs._items.at(i).getName() << ": $" << rhs._items.at(i).getPrice() << endl;
+    }
+
+    return o;
+}
