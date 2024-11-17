b3c087c0852ba947f793943d6429512ae605d116
stricker1 <stricker1392@gmail.com>
2024-11-16 16:25:57 -0800

    removed pointers

--- menu.cpp
+++ menu.cpp
@@ -5,7 +5,7 @@
 
 using namespace std;
 
-Menu::Menu(userInfo* menuOption){
+Menu::Menu(userInfo menuOption){
     user = menuOption;
 }
 
@@ -13,7 +13,7 @@ void Menu::showMenu(){
     int userInput;
     bool running = true;
 
-    cout << "HELLO " << user->getName() << endl;
+    cout << "HELLO " << user.getName() << endl;
 
     while (running) {
         userInput = 0;
@@ -44,7 +44,7 @@ void Menu::showMenu(){
 }
 
 void Menu::showBudget() {
-    user->printBudgets();
+    user.printBudgets();
 }
 
 void Menu::setBudget() {
@@ -59,21 +59,21 @@ void Menu::setBudget() {
         cout << "Input new Income: ";
         double newIncome = 0;
         cin >> newIncome;
-        user->setIncome(newIncome);
+        user.setIncome(newIncome);
     } else if (answer == 2) {
-        user->printIndexs();
+        user.printIndexs();
         cout << "Input a number for the Category you wish to edit: ";
         unsigned index = 0;
         cin >> index;
         cout << "Input a new budget %: ";
         double percent = 0;
         cin >> percent;
-        user->setPercent(percent, index);
+        user.setPercent(percent, index);
     }
 }
 
 void Menu::showExpenses() {
-    user->printIndexs();
+    user.printIndexs();
     
 }
 
